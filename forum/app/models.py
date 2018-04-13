a8366ab5622aa1556189b1d031f2213ac6a9859f
chamberijorik <chamberikjorik@gmail.com>
2018-04-13 19:13:34 +0300

    no login problem

--- forum/app/models.py
+++ forum/app/models.py
@@ -1,26 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-class User(models.Model):
-    id = models.BigIntegerField(primary_key=True)
-    name = models.TextField(max_length=30,null=True, blank=False)
-    sirname = models.TextField(max_length=30, null=True, blank=False)
-    login = models.TextField(max_length=25, null=False, unique=True)
-    password = models.CharField(max_length=128, null=False, blank=False)
-    email = models.TextField(max_length=25, null=False, blank=False, unique=True)
+
+class Profile(models.Model):
+    # id = models.BigIntegerField(primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    # username1 = models.TextField(max_length=30, null=False, unique=True)
+    bio = models.TextField(max_length=200, blank=True, null=True)
+    birth_date = models.DateField(null=True, blank=True)
     city = models.TextField(max_length=25, null=True, blank=False)
     country = models.TextField(max_length=25, null=True, blank=False)
-    avatar = models.TextField(max_length=25,null=True, blank=False) # link to the avatar image at the special folder
-    last_seen_at = models.DateTimeField() # make this update when user logs out
-    created_at = models.DateTimeField(auto_now_add=True)
+    avatar = models.TextField(max_length=25,null=True, blank=False)# TODO: FileField # link to the avatar image at the special folder
     updated_at = models.DateTimeField(auto_now=True) # last modified his profile
     confirmed = models.BooleanField(null=False, blank=False)
-    is_admin = models.BooleanField(null=False,blank=False)
-    
+
+    class Meta():
+        db_table = 'user_profile'
+
     def is_user_confirmed(self):
         return self.confirmed
     
-    def __str__(self):
-        return self.login
+    # def __str__(self):
+    #     return self.username
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-
-
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
