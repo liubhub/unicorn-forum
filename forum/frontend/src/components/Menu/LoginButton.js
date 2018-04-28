9872c4700c6133464657201fa118ff46ae7a11b9
chamberijorik <chamberikjorik@gmail.com>
2018-04-28 18:41:38 +0300

    propagation on the target element

--- forum/frontend/src/components/Menu/LoginButton.js
+++ forum/frontend/src/components/Menu/LoginButton.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import LoginForm from './LoginForm';
+// import LoginForm from './LoginForm';
+import PortalComponent from './LoginForm1';
 
 class LoginButton extends Component {
 
@@ -17,16 +18,24 @@ class LoginButton extends Component {
             <div className="button" id="login" onClick={this.handleClick}>
                 <div>Log in</div>
                 {/* передать сюда аргумент которые меняет состояние из-эктив */}
-                <LoginForm is_active={this.state.isClicked}/>
+                {/* <LoginForm is_active={this.state.isClicked}/> */}
+
+                {this.state.isClicked && (
+                    <PortalComponent onClose={() => this.setState({ isClicked: false })}>
+                            {/* <h1>This is modal content</h1> */}
+                    </PortalComponent>
+        )}
+
+
             </div>
         )
     }
 
     handleClick(event) {
         console.log('Clicked');
-        console.log(event);
-        event.stopPropagation();
-        console.log('stopped')
+        // console.log(event);
+        // event.stopPropagation();
+        // console.log('stopped')
         // event.stopPropagation ? event.stopPropagation() : (event.cancelBubble=true);
         // event.stopImmediatePropagation() ;
         this.setState({
