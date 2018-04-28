5fdf2d0c9c1e345124baa8c16e5264e70a8d9693
chamberijorik <chamberikjorik@gmail.com>
2018-04-28 19:47:44 +0300

    may be final version of prototype for modal form

--- forum/frontend/src/components/Menu/BaseModalComponent.js
+++ forum/frontend/src/components/Menu/BaseModalComponent.js
@@ -3,7 +3,7 @@ import shortid from "shortid";
 
 const uuid = shortid.generate;
 
-export function baseModalWrapper(children, onClose) {
+export function baseModalWrapper(title, children, onClose, main_button_text,add_button_text) {
 
     const fields = children.map(child =>
         <div className="column" key={uuid()}> 
@@ -14,10 +14,26 @@ export function baseModalWrapper(children, onClose) {
     const modalWindowElements = 
     <div className="modal is-active">
         <div className="modal-background"></div>
-        <div className="modal-content">
-            {fields}
+        <div className="modal-card">
+
+            <header className="modal-card-head">
+                <p class="modal-card-title">{title}</p>
+                <button class="delete" aria-label="close"></button>
+            </header>
+
+            <section class="modal-card-body">
+                {fields}
+            </section>
+
+        <footer class="modal-card-foot">
+            <button class="button is-success">{main_button_text}</button>
+            <button class="button is-text">{add_button_text}</button>
+
+        </footer>
+            
         </div>
-        <button className="modal-close is-large" aria-label="close" onClick={onClose}></button>
+
+        {/* <button className="modal-close is-large" aria-label="close" onClick={onClose}></button> */}
     </div>
 
     return modalWindowElements;
--- forum/frontend/src/components/Menu/LoginForm.js
+++ forum/frontend/src/components/Menu/LoginForm.js
@@ -6,29 +6,16 @@ import '../../../static/css/style.css'
 
 
 const onInputClick = (event) => { event.stopPropagation() };
-var fields = [baseModalInput("Name","text","Username or email", onInputClick), 
-              baseModalInput("Password","password","Password", onInputClick)]
 
-// var fields = [<div className="field">
-//             <label className="label">Name</label>
-//             <div className="control">
-//                 <input className="input" type="text" placeholder="Username or email" onClick={} />
-//             </div>
-           
-//         </div>,
-
-//         <div className="field">
-//             <label className="label">Password</label>
-//             <div className="control">
-//                 <input className="input" type="text" placeholder="Password" onClick={(event) => { event.stopPropagation() }} />
-//             </div>
-//         </div>]
-
+var fields = [
+                baseModalInput("Name","text","Username or email", onInputClick), 
+                baseModalInput("Password","password","Password", onInputClick)
+            ]
 
 const LoginPortalComponent = ({ onClose }) => {
     console.log(fields);
     return ReactDOM.createPortal(
-        baseModalWrapper(fields, onClose), document.getElementById("portal")
+        baseModalWrapper('Log In', fields, onClose, 'Log in', "Forgot Password?"), document.getElementById("portal")
     );
 };
 
