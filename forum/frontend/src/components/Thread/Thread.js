d96b4bfd247b71c18cee7c14860c3526ec9243cc
chamberijorik <chamberikjorik@gmail.com>
2018-04-30 22:06:51 +0300

    testing thread mark up

---
+++ forum/frontend/src/components/Thread/Thread.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import mui from 'material-ui';
+
+import ForumIcon from 'react-material-icons/icons/communication/forum';
+
+class Alarm extends Component {
+  render() {
+    return (
+      <ForumIcon/>
+    );
+  }
+}
+
+export default Alarm;
