2a6726ffee9a7c1e84a7c5e72a65f989047d803e
chamberijorik <chamberikjorik@gmail.com>
2018-04-30 17:49:28 +0300

    added category component

--- forum/frontend/src/components/CategoryDropdown/categoryDropdown.js
+++ forum/frontend/src/components/CategoryDropdown/categoryDropdown.js
@@ -1,9 +1,5 @@
-// // при клике необходимо поменять класс родителя на активный,
-// // подгрузить элементы категорий из бд
-// // вставить их в соответствующие дивы в родителяи 
-// // отрендереить
-
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CategoryButton extends Component {
 
@@ -11,12 +7,13 @@ class CategoryButton extends Component {
         super(props)
         this.state = {
             isActive: false,
-            hasData: false,
+            haveData: false,
         }
         this.handleClick = this.handleClick.bind(this)
     }
 
     render() {
+        console.log('State when render', this.state);
         return (
             <div className={this.state.isActive ? "dropdown is-active" : "dropdown"}>
                 <div className="dropdown-trigger">
@@ -27,67 +24,52 @@ class CategoryButton extends Component {
                         </span>
                     </button>
                 </div>
-                {this.state.hasData ? renderCategories() : ''}
+                {this.state.haveData ? this.renderCategories() : ''}
             </div>
         )
     }
 
     handleClick(event) {
-        console.log('clicked')
         this.setState({
             isActive: !this.state.isActive,
         });
+
+        if (!this.state.data) {
+
+
+            const url = '/api/category';
+
+            axios(url)
+                .then(data => {
+                    this.setState({
+                        haveData: true,
+                        data
+                    })
+                    console.log(this.state);
+                })
+                .catch(err => console.warn('Error:', err));
+        }
+    }
+
+    renderCategories() {
+        const categories = this.state.data.data;
+
+        var dropdown = categories.map(elem =>
+            <a href="#" className="dropdown-item" key={elem.id}>{elem.category_name}</a>
+        )
+
+        console.log(dropdown)
+
+        return (
+            <div className="dropdown-menu" id="dropdown-menu3" role="menu">
+                <div className="dropdown-content">
+
+                    {dropdown}
+
+                </div>
+            </div>
+        );
     }
 }
 
 export default CategoryButton;
-
-
-// // const CategoryDropdown = ({ 
-// //     fetching,
-// //     data,
-// //     reset
-// // }) => (
-// //     fetching ?
-// //     <p>Loading...</p> :
-
-// //     <div class="dropdown-menu" id="dropdown-menu3" role="menu">
-// //     <div class="dropdown-content">
-
-// //         {/* { data.map((elem) => {
-
-// //             })
-// //         } */}
-// //         data
-
-// //     </div>
-// //     </div>
-// // );
-
-
-// class CategoryButton extends Component{
-//     constructor(props){
-//         super(props);
-
-//         this.state = {
-//             isActive: false
-//         }
-
-//         this.handleClick = this.handleClick.bind(this)
-//     }
-
-//     handleClick() {
-//         this.setState({
-//             isActive: !this.state.isActve,
-//         });
-//     }
-
-//     render() {
-//         console.log("rendering category button suka");
-//         return (
-//            <div>Hello</div>
-//         )
-//     }
-// }
-
-// // {this.state.isActive ? "dropdown is-active" : "dropdown"}
