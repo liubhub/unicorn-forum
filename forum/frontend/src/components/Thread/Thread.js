e41d8dcc68d564ce742ad7e059bd633caa986df5
chamberijorik <chamberikjorik@gmail.com>
2018-05-02 17:57:48 +0300

    basic styles and standard avatar

--- forum/frontend/src/components/Thread/Thread.js
+++ forum/frontend/src/components/Thread/Thread.js
@@ -9,7 +9,18 @@ let iconStyles = {
   fontSize: '12px',
   color: 'grey'
 }
-const noneAvatarUrl = "https://bulma.io/images/placeholders/128x128.png";
+// const noneAvatarUrl = "https://bulma.io/images/placeholders/128x128.png";
+const noneAvatarUrl = '/static/avatar/avatar.png';
+const max_thread_content = 90;
+
+function dateDifference(date){
+  if(!date){
+    return '';
+  }
+  var diff = (new Date() - new Date(date));
+  diff = Math.ceil(diff / (1000 * 3600 * 24));
+  return diff > 30 ? (new Date(date)).toLocaleDateString() : diff > 1 ? ' '+diff.toString() + ' days' : 'day';
+}
 
 const MediaElement = ({thread}) => 
   !thread ? ( <div> Nothing to show </div> ) : (
@@ -23,16 +34,17 @@ const MediaElement = ({thread}) =>
     <div className="media-content">
       <div className="content">
         <div className="subject details">
-          <strong>{thread.subject}</strong> <small>{thread.author_username}</small>
-          <small>{thread.creation_date}</small>
-          <p className="details">{thread.content}</p>
-          <small>{thread.category}</small>
+          <strong>{thread.subject}</strong> <small>{'@'+thread.author_username.toString()}</small>
+          <small>{dateDifference(thread.creation_date)}</small>
+          <p className="details">{thread.content.length > max_thread_content ? thread.content.slice(0,max_thread_content) : thread.content}</p>
+          <small className="category">{thread.category}</small>
           <ForumIcon style={iconStyles}/>
           <small className="num-of-replies">{thread.number_of_replies.toString() + " replies"}</small>
         </div>
       </div>
     </div>
 
+    { thread.number_of_replies > 0 ? (
     <div className="media-right is-hidden-mobile ">
     <figure className="author-of-comment is-pulled-left "> 
       <p className="image is-64x64">
@@ -41,9 +53,12 @@ const MediaElement = ({thread}) =>
    </figure>
   <div className="author-of-comment is-pulled-right">
     <p><strong>{thread.last_commented_username}</strong></p>
-    <p><small>{thread.last_commented_date}</small></p>
+    <p><small>{dateDifference(thread.last_commented_date)}</small></p>
   </div>
-  </div>
+  </div>) : ''
+
+    }
+
     </article>
   );
 
--- forum/frontend/src/components/ThreadList/ThreadList.js
+++ forum/frontend/src/components/ThreadList/ThreadList.js
@@ -7,19 +7,19 @@ import MediaElement from '../Thread/Thread';
 const uuid = shortid.generate;
 
 function Threads(elements) {
-  const threadList = elements.map(elem => 
-    <MediaElement thread={elem}  key ={uuid()}/>
-  );
-  return (
-    <div> {threadList} </div>
-  )
+    const threadList = elements.map(elem =>
+        <MediaElement thread={elem} key={uuid()} />
+    );
+    return (
+        <div> {threadList} </div>
+    )
 }
 
 const ThreadList = () => (
-  <DataProvider endpoint="api/threads/" render={data => 
-    Threads(data)
-  } 
-  />
+    <DataProvider endpoint="api/threads/" render={data =>
+        Threads(data)
+    }
+    />
 );
 
 export default ThreadList;
