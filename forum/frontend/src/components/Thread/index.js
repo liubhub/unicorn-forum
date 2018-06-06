cd9533998da604f1c962f7d600a8d29a9700009e
chamberijorik <chamberikjorik@gmail.com>
2018-06-07 00:27:37 +0300

    fixed users avatars on rendering index page

--- forum/frontend/src/components/Thread/index.js
+++ forum/frontend/src/components/Thread/index.js
@@ -35,7 +35,6 @@ export class MediaElement extends Component {
   }
 
   handleClick(event) {
-    // TODO
     const thread_url = '/thread/' + event.currentTarget.id.toString();
 
     window.location.href = thread_url;
@@ -43,13 +42,14 @@ export class MediaElement extends Component {
   }
 
   render() {
-    console.log(this.props.thread)
+    // console.log('This')
+    // console.log(this.props.thread.user.avatar)
     // return null;
     return (!this.props.thread ? <div> Nothing to show </div> : (
       <article className="media" id={'t' + this.props.thread.entity.toString()} onClick={this.handleClick}>
         <figure className="media-left">
           <p className="image is-64x64">
-            <img src={this.props.thread.creator_avatar || noneAvatarUrl} />
+            <img src={this.props.thread.user.avatar || noneAvatarUrl} />
           </p>
         </figure>
 
@@ -89,50 +89,6 @@ export class MediaElement extends Component {
 }
 
 
-
-
-// const MediaElement = ({thread}) => 
-//   !thread ? ( <div> Nothing to show </div> ) : (
-//     <article className="media" id={'t' + thread.id.toString()}>
-//     <figure className="media-left">
-//       <p className="image is-64x64">
-//         <img src={thread.author_avatar || noneAvatarUrl} />
-//       </p>
-//     </figure>
-
-//     <div className="media-content">
-//       <div className="content">
-//         <div className="subject details">
-//           <strong>{thread.subject}</strong> <small>{'@'+thread.author_username.toString()+' '}</small>
-//           <small>{dateDifference(thread.creation_date) == 'day' ? 'today' : dateDifference(thread.creation_date)}</small>
-//           <p className="details">{thread.content.length > max_thread_content ? thread.content.slice(0,max_thread_content) : thread.content}</p>
-//           <small className="category">{thread.category}</small>
-//           <ForumIcon style={iconStyles}/>
-//           <small className="num-of-replies">{thread.number_of_replies.toString() + " replies"}</small>
-//         </div>
-//       </div>
-//     </div>
-
-//     { thread.number_of_replies > 0 ? (
-//     <div className="media-right is-hidden-mobile ">
-//     <figure className="author-of-comment is-pulled-left "> 
-//       <p className="image is-64x64">
-//         <img src={thread.last_commented_avatar || noneAvatarUrl}/>
-//       </p>
-//    </figure>
-//   <div className="author-of-comment is-pulled-right">
-//     <p><strong>{thread.last_commented_username}</strong></p>
-//     <p><small>{dateDifference(thread.last_commented_date)}</small></p>
-//   </div>
-//   </div>) : ''
-
-//     }
-
-//     </article>
-//   );
-
 MediaElement.propTypes = {
   thread: PropTypes.object.isRequired
 };
-
-// export default MediaElement;
