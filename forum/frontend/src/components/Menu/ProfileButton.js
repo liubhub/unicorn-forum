86f1bca46775788cd4851b9b7bfb140a4076539d
chamberijorik <chamberikjorik@gmail.com>
2018-06-12 18:25:36 +0300

    fixed Profile Button to href profile link

--- forum/frontend/src/components/Menu/ProfileButton.js
+++ forum/frontend/src/components/Menu/ProfileButton.js
@@ -1,19 +1,42 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
+
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
+
 
 class ProfileButton extends Component{
     constructor(props){
         super(props);
         this.state = {
-            isClicked: false
+            user: null
         }
         this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick(){
-        // нужно взять токен вложить в заголовки и послать гет-запрос
-        // /username => render profile template
-        // /api/username => json for template
+    componentWillMount(){
+        const url = 'http://127.0.0.1:8000/' + 'api/user'
+        const config = { headers: { 'Authorization': 'Bearer ' + localStorage.getItem('token') } };
+        fetch(url, config)
+            .then(response => {
+                return response.json();
+            }).then(json => {
+                this.setState({
+                    user: json
+                },function(){
+                    console.log(this.state.user.username);
+                })
+            });
+    }
 
+    handleClick(){
+        // получить имя пользователя который залогинен
+        
+        const user = this.state.user.username;
+
+        console.log(window.location.origin + "/user/" + user.toString());
+        
+        window.location.href = window.location.origin + "/user/" + user.toString()
     }
 
     render(){
