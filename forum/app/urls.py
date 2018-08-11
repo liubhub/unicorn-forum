7cba7d09321a5a4938b34365f7f9cc1606121d33
liubhub <liuhub.dev@gmail.com>
2018-08-11 16:43:49 +0300

    fix unused urls

--- forum/app/urls.py
+++ forum/app/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     url(r'^thread/(?P<thread_id>t[0-9]+)/$', views.ThreadAPI.as_view()),
     url(r'^thread/(?P<thread_id>t[0-9]+)((?P<data>.*/$))', views.ThreadAPI.as_view()),
 
-    # path('user', views.UserAPI.as_view()),
     
     url(r'^user/(?P<username>\w+)/$', views.UserAPI.as_view()),
     url(r'^user/(?P<username>\w+)/edit/$', views.edit_profile),
@@ -18,27 +17,4 @@ urlpatterns = [
 
     # TODO: 
     path('users/', views.users_template),
-
-    # path('api/users', views.users_info),
-    # url(r'thread/(?P<thread_id>t[0-9]+)((?P<data>[True]+)/)?$', views.Thread.as_view()),
-
-    # url(r'thread/(?P<thread_id>t[0-9]+)', views.Thread.as_view()),
-    # url(r'thread/(?P<thread_id>t[0-9]+)/(?P<data>[true]+)', views.Thread.as_view()),
-
-    # активация профиля пользователя
-    # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
-    # path('verify/',views.verify)
-    
-    # path('api/category/', views.CategoryListCreate.as_view()),
-    # path('api/threads/', views.threads_view),
-    # path('register/', views.register),
-    # path('login/',views.Login.as_view()),
 ]
-
-# url(r'^so/(?P<required>\d+)/$', 'myapp.so', name='something'),
-# url(r'^so/(?P<required>\d+)/(?P<optional>.*)/$', 'myapp.so', name='something_else'),
-
-# re_path(r'^activate/(?P<uidb64>\w+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     views.activate,name='activate'),
-# r'^(?P<pk>[0-9]+)/$'  [0-9A-Za-z_\-]
-# http://127.0.0.1:8000/%5Eactivate/(%3FPb'MTQ'%5B0-9A-Za-z_%5C-%5D+)/(%3FP4w1-c6157b567805eafd7a22%5B0-9A-Za-z%5D%7B1,13%7D-%5B0-9A-Za-z%5D%7B1,20%7D)/$
